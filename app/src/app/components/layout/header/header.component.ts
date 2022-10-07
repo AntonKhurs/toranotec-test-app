43017bf6cedc756168e2e69fc2ac5ea9d855ab6f
AntonKhurs <57541664+AntonKhurs@users.noreply.github.com>
2022-10-07 17:27:05 +0900

    changed layout, styles, dummy search btn in header

--- app/src/app/components/layout/header/header.component.ts
+++ app/src/app/components/layout/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { GiphyService } from 'src/app/services/giphy.service';
 
 @Component({
   selector: 'header[app-header]',
@@ -7,7 +8,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
-  constructor() { }
+  constructor(public giphyService: GiphyService,
+  ) {
+
+  }
+
+  public callGiphyService() {
+    console.log('callGiphyService');
+    this.giphyService.getGifs('test');
+  }
 
   ngOnInit(): void {
     console.log('HeaderComponent -> onInit');
